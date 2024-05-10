662034f221f4e117dd8e1693420f04032e251869
Marcos A. G. Filho <127141656+bife-33@users.noreply.github.com>
2024-05-10 13:24:23 -0300

    Ajustes na verificação da pasta scripts

--- atualizar_scripts_sge.sh
+++ atualizar_scripts_sge.sh
@@ -5,30 +5,30 @@ TRUNK=solisge_trunk
 BETA=solisge_beta
 STABLE=solisge_stable
 SCRIPTS=sge-scripts
-NOME_PASTA=".scripts"
+NOME_PASTA="scripts"
 
-if [ -d /var/docker/$TRUNK/www/$NOME_PASTA ]; then
-    rm -r /var/docker/$TRUNK/www/$NOME_PASTA
+if [ -d /var/docker/$TRUNK/www/.$NOME_PASTA ]; then
+    rm -r /var/docker/$TRUNK/www/.$NOME_PASTA
 fi
 if [ -d /var/docker/$SCRIPTS/$NOME_PASTA ]; then
-    cp -r /var/docker/$SCRIPTS/$NOME_PASTA /var/docker/$TRUNK/www/
-    chmod -Rf 777 /var/docker/$TRUNK/www/$NOME_PASTA
+    cp -r /var/docker/$SCRIPTS/$NOME_PASTA /var/docker/$TRUNK/www/.$NOME_PASTA
+    chmod -Rf 777 /var/docker/$TRUNK/www/.$NOME_PASTA
 fi
 
-if [ -d /var/docker/$BETA/www/$NOME_PASTA ]; then
-    rm -r /var/docker/$BETA/www/$NOME_PASTA
+if [ -d /var/docker/$BETA/www/.$NOME_PASTA ]; then
+    rm -r /var/docker/$BETA/www/.$NOME_PASTA
 fi
 if [ -d /var/docker/$SCRIPTS/$NOME_PASTA ]; then
-    cp -r /var/docker/$SCRIPTS/$NOME_PASTA /var/docker/$BETA/www/
-    chmod -Rf 777 /var/docker/$BETA/www/$NOME_PASTA
+    cp -r /var/docker/$SCRIPTS/$NOME_PASTA /var/docker/$BETA/www/.$NOME_PASTA
+    chmod -Rf 777 /var/docker/$BETA/www/.$NOME_PASTA
 fi
 
-if [ -d /var/docker/$STABLE/www/$NOME_PASTA ]; then
-    rm -r /var/docker/$STABLE/www/$NOME_PASTA
+if [ -d /var/docker/$STABLE/www/.$NOME_PASTA ]; then
+    rm -r /var/docker/$STABLE/www/.$NOME_PASTA
 fi
 if [ -d /var/docker/$SCRIPTS/$NOME_PASTA ]; then
-    cp -r /var/docker/$SCRIPTS/$NOME_PASTA /var/docker/$STABLE/www/
-    chmod -Rf 777 /var/docker/$STABLE/www/$NOME_PASTA
+    cp -r /var/docker/$SCRIPTS/$NOME_PASTA /var/docker/$STABLE/www/.$NOME_PASTA
+    chmod -Rf 777 /var/docker/$STABLE/www/.$NOME_PASTA
 fi
 
 echo ""
